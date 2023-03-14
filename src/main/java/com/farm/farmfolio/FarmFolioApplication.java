968d0db31fccc229263e9fe0ed266c297fedfae3
Mounish G <mounish.g@calpyte.com>
2023-03-14 15:54:38 +0530

    initial commit

---
+++ src/main/java/com/farm/farmfolio/FarmFolioApplication.java
@@ -0,0 +1,13 @@
+package com.farm.farmfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmFolioApplication.class, args);
+	}
+
+}
